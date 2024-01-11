1f82d8696dbd6eb50204f00618c537b55ba91660
leondobson <148569437+leondobson@users.noreply.github.com>
2024-01-11 14:49:38 +0000

    defining functions

--- script.js
+++ script.js
@@ -4,3 +4,30 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+const generateTarget = () => {
+    return Math.floor(Math.random() * 10);
+}
+
+const compareGuesses = (userGuess, computerGuess, target) => {
+    let userDifference = Math.abs(target - userGuess);
+    let computerDifference = Math.abs(target - computerGuess);
+
+    if (userDifference <= computerDifference) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const updateScore = (winner) => {
+    if (winner = "human") {
+        humanScore += 1
+    } else if (winner = "computer") {
+        computerScore += 1
+    }
+}
+
+const advanceRound = () => {
+    currentRoundNumber += 1
+}
+
